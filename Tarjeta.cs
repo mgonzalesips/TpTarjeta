708347c8a34122d502dd57476e8e37427c98870e
Bautista <bautistajosebona@gmail.com>
2024-09-27 21:45:40 -0300

    Franquicias de Boleto actualizadas

--- Tarjeta.cs
+++ Tarjeta.cs
@@ -1,92 +1,68 @@
 using System;
 
-public class Tarjeta{
-  //Constantes propias del sistema de la tarjeta
-  private const float tarifa_basica = 940;
-  private const float saldo_max = 9900;
+namespace TpSube {
+  public class Tarjeta {
+    private const float saldo_max = 9900;
+    private const float saldo_negativo_max = -480;
+    
+    public float saldo { get; protected set; }
 
-  public float saldo{ get; private set; }
-
-  //Metodo para crear una tarjeta con su saldo
-  public Tarjeta (float saldo_inicial){
-    if(saldo_inicial > saldo_max){
-      saldo = saldo_max;
-      Console.WriteLine("Saldo inicial excede el saldo maximo, se ha cargado 9400"); 
-    }
-    else{
-      saldo = saldo_inicial;
-    }
-  }
-
-  //Metodo para cargar saldo a la tarjeta, devolviendo true o false dependiendo si la operacion fue existosa o no
-  public bool CargarSaldo(float monto_carga) {
-    if(EsCargaValida(monto_carga)){        //Se verifica si el monto de carga es valido
-      float nuevo_saldo = saldo + monto_carga;
-      if(nuevo_saldo > saldo_max){         //Se verifica si el nuevo saldo es valido
-        float monto_sobrante = nuevo_saldo - saldo_max;
-        Console.WriteLine($"Saldo maximo excedido por {monto_sobrante}$");
-        return false;
+    public Tarjeta(float saldo_inicial) {
+      if (saldo_inicial > saldo_max) {
+        saldo = saldo_max;
+        Console.WriteLine("Saldo inicial excede el saldo máximo, se ha cargado $9900"); 
+      } else {
+        saldo = saldo_inicial;
       }
-      else{
-        saldo = nuevo_saldo;
-        Console.WriteLine($"Su tarjeta ha sido cargada, su saldo es de {saldo}$"); 
+    }
+    
+    public void actualizar_saldo(float monto_a_actualizar){
+      saldo = saldo - monto_a_actualizar;
+    }  
+
+    public bool CargarSaldo(float monto_carga) {
+      if (EsCargaValida(monto_carga)) {
+        if (saldo < 0) {
+          float deuda = Math.Abs(saldo);
+          if (monto_carga > deuda) {
+            saldo = monto_carga - deuda;  
+            Console.WriteLine($"Se descontaron ${deuda} de la deuda. Su nuevo saldo es de ${saldo}");
+          } else {
+            saldo += monto_carga;  
+            Console.WriteLine($"Se descontaron ${monto_carga} de la deuda. Su deuda restante es de ${Math.Abs(saldo)}");
+          }
+        } else {
+          saldo += monto_carga; 
+        }
+        if (saldo > saldo_max) {
+          float monto_sobrante = saldo - saldo_max;
+          saldo = saldo_max;
+          Console.WriteLine($"Saldo máximo excedido por ${monto_sobrante}");
+          return false;
+        }
+        Console.WriteLine($"Su tarjeta ha sido cargada, su saldo es de ${saldo}");
         return true;
       }
-    }
-    else{
       return false;
     }
-  }
 
-  //Metodo para el pago del pasaje
-   public virtual bool PagarPasaje() {
-    if (saldo >= tarifa_basica) {
-      saldo -= tarifa_basica;
-      return true;
+    private bool EsCargaValida(float monto_carga) {
+      return monto_carga == 2000 || monto_carga == 3000 || monto_carga == 4000 ||
+             monto_carga == 5000 || monto_carga == 6000 || monto_carga == 7000 ||
+             monto_carga == 8000 || monto_carga == 9000;
     }
-    return false; // Si el saldo es insuficiente
   }
 
-  //Verificar si el monto de carga es un monto válido
-  private bool EsCargaValida(float monto_carga){
-    return monto_carga == 2000 || monto_carga == 3000 || monto_carga == 4000 ||
-     monto_carga == 5000 || monto_carga == 6000 || monto_carga == 7000 ||
-     monto_carga == 8000 || monto_carga == 9000;
-  }
-  
   public class MedioBoleto : Tarjeta {
-    private const float tarifa_medio_boleto = tarifa_basica / 2;
-
     public MedioBoleto(float saldo_inicial) : base(saldo_inicial) {}
 
-    public override bool PagarPasaje() {
-      if (saldo >= tarifa_medio_boleto || saldo >= Tarjeta.saldo_negativo_max) {
-        saldo -= tarifa_medio_boleto;
-        return true;
-      }
-      return false;
-    }
   }
 
   public class GratuitoJubilados : Tarjeta {
     public GratuitoJubilados(float saldo_inicial) : base(saldo_inicial) {}
-
-    public override bool PagarPasaje() {
-      Console.WriteLine("Pasaje gratuito.");
-      Console.WriteLine($"Saldo restante {saldo}");
-      return true;
-    }
   }
 
   public class GratuitoEstudiantes : Tarjeta {
     public GratuitoEstudiantes(float saldo_inicial) : base(saldo_inicial) {}
-
-    public override bool PagarPasaje() {
-      Console.WriteLine("Pasaje gratuito.");
-      Console.WriteLine($"Saldo restante {saldo}");
-      return true;
-    }
   }
-  
-  
 }
